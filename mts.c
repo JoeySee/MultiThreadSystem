32ac9d2094540866da8bd6e61e9a471e87d7202b
Joey Seeman <josephseeman@linux202.csc.uvic.ca>
2025-10-20 10:47:02 -0700

    Train process struct created

--- mts.c
+++ mts.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-pthread_cond_t timer_start, 
+pthread_cond_t loading_done, sending_done, timer_start;
+pthread_mutex_t east_queue, west_queue, standby, crossing; 
 
 struct train{
-
+    int loading_time;
+    int crossing_time;
+    char direction;
+    int is_high_priority;
+    struct train* previous;
+    int* standby_count;
 }
 
 int main(int argc, char** argv)
@@ -16,11 +22,16 @@ int main(int argc, char** argv)
         exit(1);
     }
     FILE* fptr = fopen(argv[1], "r");
-    char buffer[100];
+    char buffer[20];
     fscanf(fptr, "%s", buffer);
     printf("%s\n", buffer);
     fscanf(fptr, "%s", buffer);
     printf("%s\n", buffer);
 
     // Train creation
+    while(buffer != NULL)
+    {
+
+        fscanf(fptr, buffer);
+    }// while
 }// main 
